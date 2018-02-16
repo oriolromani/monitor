92b44fe9e2609f18fbeb2990bb68ad1c52d985fa
Oriol Romaní <oriol.romani@upf.edu>
2018-02-16 10:32:16 +0100

    Update settings.py

--- fingerprint/settings.py
+++ fingerprint/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'fingerprint.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bmat',
+        'NAME': 'db',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
